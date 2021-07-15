56151225edfadd5e79cd98d3f1dc3c7fb35ce504
André <andre1993andre@hotmail.com>
2021-07-14 22:04:20 -0300

    Exercicio de fixação

---
+++ ExercicioDeFixacao1.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+
+public class ExercicioDeFixacao1 {
+
+	public static void main(String[] args) {
+
+		String product1 = "computer";
+		String product2 = "Office desk";
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+
+		System.out.println("Products: ");
+		System.out.printf("%s, which price is $ %.2f %n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f %n",product2, price2);
+		System.out.println(" ");
+		System.out.printf("Record: %d years old, code %d and gender: %s %n", age, code, gender);
+		System.out.println(" ");
+		System.out.printf("measue whit eigth decimal places %.8f %n ",measure);
+		System.out.printf("rouded (three decimal places): %.3f %n", measure);
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f %n", measure);
+	}
+
+}
